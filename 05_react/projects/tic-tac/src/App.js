28a71a230dc070ab7ebce67f1bd08235e20b4ecb
Pengfei Liu <liu.pengfei@hotmail.fr>
2024-03-28 10:20:03 +0100

    add tic tac

---
+++ 05_react/projects/tic-tac/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+
+// This function takes an argument, it creates the quare with the value in it
+function Square({ value }) {
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+function App() {
+  return (
+
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
+}
+
+// Speicfy the main component is App
+export default App;
