dc40821ba6eb24c631c78ce39ffde1b753a3b15e
Pengfei Liu <liu.pengfei@hotmail.fr>
2024-03-28 15:54:20 +0100

    update app

--- 05_react/projects/keycloak_client/src/App.js
+++ 05_react/projects/keycloak_client/src/App.js
@@ -1,11 +1,21 @@
+import React, { useState } from 'react';
+import './App.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import '/node_modules/primeflex/primeflex.css'
+import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
+
 import Keycloak from 'keycloak-js';
 
 let initOptions = {
   url: 'http://keycloak.casd.local/',
-  realm: 'master',
+  realm: 'examples',
   clientId: 'react-client',
   onLoad: 'check-sso', // check-sso | login-required
   KeycloakResponseType: 'code',
+
+  // silentCheckSsoRedirectUri: (window.location.origin + "/silent-check-sso.html")
 }
 
 let kc = new Keycloak(initOptions);
@@ -13,8 +23,7 @@ let kc = new Keycloak(initOptions);
 kc.init({
   onLoad: initOptions.onLoad,
   KeycloakResponseType: 'code',
-  silentCheckSsoRedirectUri: window.location.origin + "/silent-check-sso.html", 
-  checkLoginIframe: false,
+  silentCheckSsoRedirectUri: window.location.origin + "/silent-check-sso.html", checkLoginIframe: false,
   pkceMethod: 'S256'
 }).then((auth) => {
   if (!auth) {
@@ -40,7 +49,7 @@ function App() {
       {/* <Auth /> */}
       <div className='grid'>
         <div className='col-12'>
-          <h1>My Awesome React App</h1>
+          <h1>React App with keycloak</h1>
         </div>
         <div className='col-12'>
           <h1 id='app-header-2'>Secured with Keycloak</h1>
@@ -55,7 +64,7 @@ function App() {
           <Button onClick={() => { setInfoMessage(JSON.stringify(kc.tokenParsed)) }} className="m-1" label='Show Parsed Access token' severity="info" />
           <Button onClick={() => { setInfoMessage(kc.isTokenExpired(5).toString()) }} className="m-1" label='Check Token expired' severity="warning" />
           <Button onClick={() => { kc.updateToken(10).then((refreshed)=>{ setInfoMessage('Token Refreshed: ' + refreshed.toString()) }, (e)=>{setInfoMessage('Refresh Error')}) }} className="m-1" label='Update Token (if about to expire)' />  {/** 10 seconds */}
-          <Button onClick={() => { kc.logout({ redirectUri: 'http://localhost:3000/' }) }} className="m-1" label='Logout' severity="danger" />
+          <Button onClick={() => { kc.logout({ redirectUri: 'http://10.50.2.80:3000/' }) }} className="m-1" label='Logout' severity="danger" />
           
         </div>
       </div>
