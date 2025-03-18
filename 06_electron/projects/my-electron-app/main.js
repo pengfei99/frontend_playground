b2b07bf1197d80bd89521a79ecec594328e360d3
pengfei <liu.pengfei@hotmail.fr>
2025-03-18 10:43:01 +0100

    update project

--- 06_electron/projects/my-electron-app/main.js
+++ 06_electron/projects/my-electron-app/main.js
@@ -1,13 +1,20 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    icon: path.join(__dirname, 'assets', 'bouclier.svg'),
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+      },
   })
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
+  // Remove default menu bar
+  Menu.setApplicationMenu(null);
 }
 
 app.whenReady().then(() => {
