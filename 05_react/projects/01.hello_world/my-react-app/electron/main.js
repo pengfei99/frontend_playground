414f6064646d7870a85f74100fbb0d85cb253567
pengfei liu <liu.pengfei@hotmail.fr>
2025-06-09 10:09:40 +0200

    add electron support

---
+++ 05_react/projects/01.hello_world/my-react-app/electron/main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true,
+    },
+  });
+
+  // Load React app (Vite dev server during development)
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
